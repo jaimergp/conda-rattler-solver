c903b21d40f5ff68dbb70a78101bf4be18013f08
jaimergp <jaimergp@users.noreply.github.com>
2024-06-10 19:20:17 +0200

    fixes thanks to 0.6.3

--- conda_rattler_solver/solver.py
+++ conda_rattler_solver/solver.py
@@ -2,31 +2,21 @@ import asyncio
 from functools import lru_cache
 from pathlib import Path
 
-from boltons.setutils import IndexedSet
+import rattler
+from conda.base.constants import ChannelPriority
 from conda.base.context import context
 from conda.common.constants import NULL
 from conda.common.io import Spinner
-from conda.core.subdir_data import SubdirData
-from conda.models.channel import Channel
 from conda.models.match_spec import MatchSpec
-from conda.models.prefix_graph import PrefixGraph
 from conda.models.records import PackageRecord
 from conda_libmamba_solver.solver import LibMambaSolver
 from conda_libmamba_solver.state import SolverInputState, SolverOutputState
 from rattler import __version__ as rattler_version
-from rattler import (
-    solve,
-    MatchSpec as RattlerMatchSpec,
-    PrefixRecord as RattlerPrefixRecord,
-    VirtualPackage,
-)
 from rattler.exceptions import SolverError as RattlerSolverError
 
 from . import __version__
 from .exceptions import RattlerUnsatisfiableError
 from .index import RattlerIndexHelper
-
-from . import __version__
 
 
 class RattlerSolver(LibMambaSolver):
@@ -98,47 +88,63 @@ class RattlerSolver(LibMambaSolver):
 
         return out_state.current_solution
 
-    def _solve_attempt(self, in_state, out_state, index):
+    def _solve_attempt(
+        self, in_state: SolverInputState, out_state: SolverOutputState, index: RattlerIndexHelper
+    ):
         out_state.check_for_pin_conflicts(index)
         tasks = self._specs_to_tasks(in_state, out_state)
         # TODO: This is a hack to get the installed packages into the solver
-        # but rattler doesn't allow PrefixRecords to be passed in yet
-        # rattler_installed = {}
-        # for json_path in Path(self.prefix).glob("conda-meta/*.json"):
-        #     name = json_path.stem.rsplit("-", 2)[0]
-        #     record = RattlerPrefixRecord.from_path(json_path)
-        #     rattler_installed[name] = record
+        # but rattler doesn't allow PrefixRecords to be built through the API yet
+        rattler_installed = {}
+        for json_path in Path(self.prefix).glob("conda-meta/*.json"):
+            name = json_path.stem.rsplit("-", 2)[0]
+            record = rattler.PrefixRecord.from_path(json_path)
+            rattler_installed[name] = record
 
         specs = []
-        pins = []
-        locked = []
+        pinned_packages = []
+        locked_packages = []
         for (task_name, _), task_specs in tasks.items():
             if task_name in ("INSTALL", "UPDATE"):
                 specs.extend(task_specs)
-            # # TODO
-            # elif task_name in ("ADD_PIN", "USERINSTALLED"):
-            #     for spec in task_specs:
-            #         for record in in_state.installed.values():
-            #             if MatchSpec(spec).match(record):
-            #                 pins.append(rattler_installed[record.name])
-            # elif task_name == "LOCK":
-            #     for spec in task_specs:
-            #         for record in in_state.installed.values():
-            #             if MatchSpec(spec).match(record):
-            #                 locked.append(rattler_installed[record.name])
+            elif task_name in ("ADD_PIN", "USERINSTALLED"):
+                for spec in task_specs:
+                    for record in in_state.installed.values():
+                        if MatchSpec(spec).match(record):
+                            pinned_packages.append(rattler_installed[record.name])
+            elif task_name == "LOCK":
+                for spec in task_specs:
+                    for record in in_state.installed.values():
+                        if MatchSpec(spec).match(record):
+                            locked_packages.append(rattler_installed[record.name])
+        virtual_packages = []
+        for pkg in in_state.virtual.values():
+            virtual_packages.append(
+                rattler.GenericVirtualPackage(
+                    rattler.PackageName(pkg.name), rattler.Version(pkg.version), pkg.build
+                )
+            )
 
         return asyncio.run(
-            solve(
-                channels=[str(info.local_json) for info in index._index.values()],
-                specs=[RattlerMatchSpec(s) for s in specs],
-                # locked_packages=locked,  # TODO
-                # pinned_packages=pins,  # TODO
-                virtual_packages=[p.into_generic() for p in VirtualPackage.current()],
+            rattler.solve_with_sparse_repodata(
+                specs=[rattler.MatchSpec(s) for s in specs],
+                sparse_repodata=[info.repo for info in index._index.values()],
+                locked_packages=locked_packages,
+                pinned_packages=pinned_packages,
+                virtual_packages=virtual_packages,
+                channel_priority=rattler.ChannelPriority.Strict
+                if context.channel_priority == ChannelPriority.STRICT
+                else rattler.ChannelPriority.Disabled,
+                strategy="highest",
             )
         )
 
     def _export_solved_records(self, records, out_state):
         for record in records:
+            if timestamp := record.timestamp:
+                timestamp = int(timestamp.timestamp() * 1000)
+            else:
+                timestamp = 0
             out_state.records[record.name] = PackageRecord(
                 name=record.name.source,
                 version=str(record.version),
@@ -158,12 +164,12 @@ class RattlerSolver(LibMambaSolver):
                 constrains=record.constrains or (),
                 track_features=record.track_features or (),
                 features=record.features or (),
-                # noarch=record.noarch,  #! TODO: MISSING
+                noarch=record.noarch,
                 # preferred_env=record.preferred_env,
                 license=record.license,
                 license_family=record.license_family,
                 # package_type=record.package_type,
-                timestamp=record.timestamp or 0,
+                timestamp=timestamp,
                 # date=record.date,
                 size=record.size or 0,
             )
