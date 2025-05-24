4daab4790854fdcbd3d06b3b929f6f96e70f026a
jaimergp <jaimergp@users.noreply.github.com>
2025-05-24 16:10:10 +0200

    update __init__

--- conda_rattler_solver/__init__.py
+++ conda_rattler_solver/__init__.py
@@ -1,1 +1,5 @@
-__version__ = "0.0.1"
+try:
+    from ._version import __version__
+except ImportError:
+    # _version.py is only created after running `pip install`
+    __version__ = "0.0.0.dev0+placeholder"
